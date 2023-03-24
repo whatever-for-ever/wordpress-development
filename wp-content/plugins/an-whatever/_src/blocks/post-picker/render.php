25b0596bd45c70c8c717790f6b344d6502825fed
Whatever Forever <whateverforever378@gmail.com>
2023-03-24 15:36:28 +0200

    Small HTML tag and CSS changes

--- wp-content/plugins/an-whatever/_src/blocks/post-picker/render.php
+++ wp-content/plugins/an-whatever/_src/blocks/post-picker/render.php
@@ -5,38 +5,36 @@ if ( ! isset( $attributes['postListId'] ) || ! isset( $attributes['postList'] ) 
 ?>
 
 <section <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>>
-	<ul>
-		<?php
-		$current_post_id = get_the_ID();
+	<?php
+	$current_post_id = get_the_ID();
 
-		foreach ( $attributes['postList'] as $key => $post_list_id ) {
-			if ( (int) $current_post_id === (int) $post_list_id ) {
-				// We don't want an endless loop.
-				continue;
-			}
-
-			$item_id = $post_list_id . '-' . $attributes['postListId'];
-			?>
-			<div class="accordion-item">
-				<h3>
-					<button
-						type="button"
-						id="accordion-item-btn-<?php echo esc_attr( $item_id ); ?>"
-						class="accordion-trigger"
-						aria-controls="accordion-item-content-<?php echo esc_attr( $item_id ); ?>"
-						aria-expanded="false"
-					>
-						<span class="accordion-title"><?php echo wp_kses_post( get_the_title( (int) $post_list_id ) ); ?></span>
-					</button>
-				</h3>
-				<div id="accordion-item-content-<?php echo esc_attr( $item_id ); ?>" role="region" aria-labelledby="accordion-item-btn-<?php echo esc_attr( $item_id ); ?>" class="accordion-panel clear-fix" hidden="">
-					<?php
-					echo wp_kses_post( apply_filters( 'the_content', get_the_content( null, false, (int) $post_list_id ) ) );
-					?>
-				</div>
-			</div>
-			<?php
+	foreach ( $attributes['postList'] as $key => $post_list_id ) {
+		if ( (int) $current_post_id === (int) $post_list_id ) {
+			// We don't want an endless loop.
+			continue;
 		}
+
+		$item_id = $post_list_id . '-' . $attributes['postListId'];
 		?>
-	</ul>
+		<div class="accordion-item">
+			<h3>
+				<button
+					type="button"
+					id="accordion-item-btn-<?php echo esc_attr( $item_id ); ?>"
+					class="accordion-trigger"
+					aria-controls="accordion-item-content-<?php echo esc_attr( $item_id ); ?>"
+					aria-expanded="false"
+				>
+					<span class="accordion-title"><?php echo wp_kses_post( get_the_title( (int) $post_list_id ) ); ?></span>
+				</button>
+			</h3>
+			<div id="accordion-item-content-<?php echo esc_attr( $item_id ); ?>" role="region" aria-labelledby="accordion-item-btn-<?php echo esc_attr( $item_id ); ?>" class="accordion-panel clear-fix" hidden="">
+				<?php
+				echo wp_kses_post( apply_filters( 'the_content', get_the_content( null, false, (int) $post_list_id ) ) );
+				?>
+			</div>
+		</div>
+		<?php
+	}
+	?>
 </section>
